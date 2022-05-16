734974889848a0793fa8962256b206773d18fac0
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2022-05-16 20:33:43 +0100

    added a feature from CYF blocks

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("image").addEventListener("click", (event) => {
+  let element_img = document.getElementById("img");
+  element_img.setAttribute(
+    "src",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNZe07P_k6QpgwJ-EWsH9XDk2X4X_jUTnSng&usqp=CAU"
+  );
+});
